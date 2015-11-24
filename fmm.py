1a6d1d3c5c17c88d0e6ec3f074a213f352ff38c3
Victor Churchill <vchurchill2008@gmail.com>
2015-11-24 12:40:02 -0500

    add circle target points

--- fmm.py
+++ fmm.py
@@ -4,7 +4,7 @@ import scipy as sp
 import math as m
 import matplotlib.pyplot as plt
 
-# create random matrix
+# create sources in box
 sources = np.random.rand(20,3)
 
 # initialize other matrices with zeros
@@ -22,12 +22,33 @@ for i in range(0,20):
   z = sources[i,2]
   xs[i] = x
   ys[i] = y
+  zs[i] = z
   r[i] = np.sqrt(m.pow(x,2) + m.pow(y,2))
   theta[i] = np.arctan(y/x)
   csources[i,0] = r[i]
   csources[i,1] = theta[i]
   csources[i,2] = z
 
-target = [3,1.4,4]
+# create targets on 
+rad = 1.25  #radius
+n = 20 #points to generate
+xt = [
+  (rad * m.cos(t)+ 0.5)
+  for t in (m.pi*2 * i/n for i in range(n))
+]
+yt = [
+  (rad * m.sin(t)+0.5)
+  for t in (m.pi*2 * i/n for i in range(n))
+]
 
-distance = 
+# plt.scatter(xs,ys)
+# plt.scatter(xt,yt)
+# plt.show()
+
+f = np.zeros(shape=(20,1))
+
+for i in range (0,20):
+  f[i] = np.log(r[i]*r[i] + xt[i]*xt[i] 
+  - 2*r[i]*xt[i]*m.cos(theta[i]) + zs[i]*zs[i])
+
+print(f)
