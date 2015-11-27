99f774e201d41bfc326cedad2466d67015b0cd69
Victor Churchill <vchurchill2008@gmail.com>
2015-11-27 15:44:02 -0500

    cleanup 2d

--- fmm.py
+++ fmm.py
@@ -1,54 +1,46 @@
 # import necessary packages
 import numpy as np
 import scipy as sp
-import math as m
+import math
 import matplotlib.pyplot as plt
 
+
+# number of sources
+n = 20
+
 # create sources in box
-sources = np.random.rand(20,3)
+sources = np.random.rand(n,2)
 
-# initialize other matrices with zeros
-xs = np.zeros(shape=(20,1))
-ys = np.zeros(shape=(20,1))
-zs = np.zeros(shape=(20,1))
-r = np.zeros(shape=(20,1))
-theta = np.zeros(shape=(20,1))
-csources = np.zeros(shape=(20,3))
+# initialize coordinate matrices with zeros
+rs = np.zeros(shape=(n,1))
+zs = np.zeros(shape=(n,1))
 
-# 
-for i in range(0,20):
-  x = sources[i,0]
-  y = sources[i,1]
-  z = sources[i,2]
-  xs[i] = x
-  ys[i] = y
+# separate coordinates of source points
+for i in range(0,n):
+  r = sources[i,0]
+  z = sources[i,1]
+  rs[i] = r
   zs[i] = z
-  r[i] = np.sqrt(m.pow(x,2) + m.pow(y,2))
-  theta[i] = np.arctan(y/x)
-  csources[i,0] = r[i]
-  csources[i,1] = theta[i]
-  csources[i,2] = z
 
-# create targets on 
-rad = 1.25  #radius
-n = 20 #points to generate
-xt = [
-  (rad * m.cos(t)+ 0.5)
-  for t in (m.pi*2 * i/n for i in range(n))
+# create targets on check surface
+# radius
+radius = 1.25
+# numbers of target points to generate
+m = 20
+rt = [
+  (radius * math.cos(t)+ 0.5)
+  for t in (math.pi*2 * i/m for i in range(m))
 ]
-yt = [
-  (rad * m.sin(t)+0.5)
-  for t in (m.pi*2 * i/n for i in range(n))
+zt = [
+  (radius * math.sin(t)+0.5)
+  for t in (math.pi*2 * i/m for i in range(m))
 ]
 
-# plt.scatter(xs,ys)
-# plt.scatter(xt,yt)
-# plt.show()
+K = np.zeros(shape=(n,m))
 
-f = np.zeros(shape=(20,1))
+for j in range(0,m):
+  for i in range(0,n):
+    K[i,j] = np.log(math.sqrt((rs[i] - rt[j])*(rs[i]-rt[j])
+    + (zs[i]-zt[j])*(zs[i]-zt[j])))
 
-for i in range (0,20):
-  f[i] = np.log(r[i]*r[i] + xt[i]*xt[i] 
-  - 2*r[i]*xt[i]*m.cos(theta[i]) + zs[i]*zs[i])
-
-print(f)
+print(K)
