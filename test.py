770925f367e72de30a2817e63bd74a7c53282a2e
Victor Churchill <vchurchill2008@gmail.com>
2015-12-04 17:11:19 -0500

    test

---
+++ test.py
@@ -0,0 +1,194 @@
+# import necessary packages
+import numpy as np
+import numpy.linalg
+import math
+import matplotlib.pyplot as plt
+
+# N is the number of sources in each box
+N1 = 20
+N2 = 20
+
+# p is the number of discretization points on the surfaces
+p = 16
+
+# 2*rb is the side length of each box
+# for now, the box sizes are the same
+rb = 0.5
+
+# x,y is the position of the bottom left hand corner of the box
+# change these to get different boxes
+x1 = 0
+y1 = 0
+x2 = 2
+y2 = 0
+
+# c is the center of each box
+c1 = [rb + x1, rb + y1]
+c2 = [rb + x2, rb + y2]
+
+# create sources for box 1
+sources1 = np.random.rand(N1,2)
+sources1 = sources1 + [x1, y1]
+
+# create sources for box 2
+sources2 = np.random.rand(N2,2)
+sources2 = sources2 + [x2, y2]
+
+# create some density for each source. Ying says this is "given"
+# note: I wasn't sure what to put as these values
+# just did random integers from 0 to 10
+phi1 = np.random.randint(10, size=(N1,1))
+phi2 = np.random.randint(10, size=(N2,1))
+
+# initialize coordinate vectors
+# box 1 sources
+rs1 = np.zeros(shape=(N1,1))
+zs1 = np.zeros(shape=(N1,1))
+# box 1 check surface
+rc1 = np.zeros(shape=(p,1))
+zc1 = np.zeros(shape=(p,1))
+# box 1 equiv surface
+rq1 = np.zeros(shape=(p,1))
+zq1 = np.zeros(shape=(p,1))
+# box 2 sources
+rs2 = np.zeros(shape=(N2,1))
+zs2 = np.zeros(shape=(N2,1))
+# box 2 check surface
+rc2 = np.zeros(shape=(p,1))
+zc2 = np.zeros(shape=(p,1))
+# box 3 equiv surface
+rq2 = np.zeros(shape=(p,1))
+zq2 = np.zeros(shape=(p,1))
+
+# initiate kernel matrices
+K1 = np.zeros(shape=(p,N1))
+K2 = np.zeros(shape=(p,N2))
+K3 = np.zeros(shape=(p,p))
+K4 = np.zeros(shape=(p,p))
+K5 = np.zeros(shape=(p,p))
+K6 = np.zeros(shape=(p,p))
+
+# initiate upward check potential vectors
+q1u = np.zeros(shape=(p,1))
+q2u = np.zeros(shape=(p,1))
+
+# initialize downward check potential vectors
+q1d = np.zeros(shape=(p,1))
+
+# break down source points into source coordinate vectors
+for i in range(0,N1):
+  rs1[i] = sources1[i,0]
+  zs1[i] = sources1[i,1]
+  
+for i in range(0,N2):
+  rs2[i] = sources2[i,0]
+  zs2[i] = sources2[i,1]
+
+# define surface constant
+d=0.1
+# radii for surfaces
+radius1 = (4-math.sqrt(2)-2*d)*rb
+radius2 = (math.sqrt(2)+d)*rb
+
+# box 1 upward check surface and downward equivalent surface vectors
+for i in range(0,p):
+  rc1[i] = radius1 * math.cos(math.pi*2 * i/p) + c1[0]
+  zc1[i] = radius1 * math.sin(math.pi*2 * i/p) + c1[1]
+
+# box 1 upward equivalent surface and downward check surface vectors
+for i in range(0,p):
+  rq1[i] = radius2 * math.cos(math.pi*2 * i/p) + c1[0]
+  zq1[i] = radius2 * math.sin(math.pi*2 * i/p) + c1[1]
+
+# box 2 upward check surface and downward equivalent surface vectors
+for i in range(0,p):
+  rc2[i] = radius1 * math.cos(math.pi*2 * i/p) + c2[0]
+  zc2[i] = radius1 * math.sin(math.pi*2 * i/p) + c2[1]
+
+# box 2 upward equivalent surface and downward check surface vectors
+for i in range(0,p):
+  rq2[i] = radius2 * math.cos(math.pi*2 * i/p) + c2[0]
+  zq2[i] = radius2 * math.sin(math.pi*2 * i/p) + c2[1]
+
+# check that this is actually points in a box with a circle around it
+# sources
+plt.scatter(rs1,zs1,color='black')
+plt.scatter(rs2,zs2,color='black')
+# upward equivalent surfaces
+plt.scatter(rq1,zq1,color='red')
+plt.scatter(rq2,zq2,color='red')
+# upward check surfaces
+plt.scatter(rc1,zc1,color='blue')
+plt.scatter(rc2,zc2,color='blue')
+#plt.show()
+# uncomment the line above to show the plot
+# to continue the program after the plot, you must close the plot window
+
+# calculate kernel matrices
+
+# green's function at rt1,zt1 on the upward check surface and rs1,zs1 sources
+# p x N1 matrix
+for i in range(0,p):
+  for j in range(0,N1):
+    K1[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rc1[i]-rs1[j])+
+    np.square(zc1[i]-zs1[j])))
+
+# green's function at rt2,zt2 on the upward check surface and rs2,zs2 sources
+# p x N2 matrix
+for i in range(0,p):
+  for j in range(0,N2):
+    K2[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rc2[i]-rs2[j])+
+    np.square(zc2[i]-zs2[j])))
+
+# green's function at rt1,zt1 on up check surf & rq1,zq1 on up equiv surface
+# p x p matrix
+for i in range(0,p):
+  for j in range(0,p):
+    K3[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rc1[i]-rq1[j])+
+    np.square(zc1[i]-zq1[j])))
+
+# green's function at rt2,zt2 on up check surf & rq2,zq2 on up equiv surface
+# p x p matrix
+for i in range(0,p):
+  for j in range(0,p):
+    K4[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rc2[i]-rq2[j])+
+    np.square(zc2[i]-zq2[j])))
+
+# green's function at rq1,zq1 on down check surf & rq2,zq2 on up equiv surface
+# p x p matrix
+for i in range(0,p):
+  for j in range(0,p):
+    K5[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rq1[i]-rq2[j])+
+    np.square(zq1[i]-zq2[j])))
+
+# green's function at rq1,zq1 on down check surf & rt1,zt1 on down equiv surface
+# p x p matrix
+for i in range(0,p):
+  for j in range(0,p):
+    K6[i,j] = (1/(2*math.pi))*np.log(math.sqrt(np.square(rq1[i]-rc1[j])+
+    np.square(zq1[i]-zc1[j])))
+
+# now solve for the upward check potentials
+# q = K*phi
+q1u = np.dot(K1,phi1)
+q2u = np.dot(K2,phi2)
+
+# solve the integral eq'n for the upward equivalent densities
+# 2piR/p * K * eqd = q
+eqd1u = p*np.dot(np.linalg.inv(K3),q1u)/(2*math.pi*radius2)
+eqd2u = p*np.dot(np.linalg.inv(K4),q2u)/(2*math.pi*radius2)
+
+# now for the M2L translation operator
+# if box 2 is in the far field of box 1
+# that is, use box 2 upward equiv density to find
+# the downward equivalent density of box 1
+
+# compute the downward check potential for box 1
+# K * eqd = q
+q1d = np.dot(K5,eqd2u)
+
+# then compute the downward equivalent density
+# 2piR/p * K * eqd = q
+eqd1d = p*np.dot(np.linalg.inv(K6),q1d)/(2*math.pi*radius1)
+
+print(eqd1d)
