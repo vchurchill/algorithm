080cc448ee6821542b02fd79f4ced7588265df32
Victor Churchill <vchurchill2008@gmail.com>
2015-12-04 17:13:45 -0500

    delete old stuff

--- 2Dkifmm-S2M.py
+++
@@ -1,8 +0,0 @@
-# import necessary packages
-#import numpy as np
-#import scipy as sp
-#import math
-#import matplotlib.pyplot as plt
-from checkpotential import checkpotential
-
-checkpotential(20,10,0.5,)
