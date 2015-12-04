c92b1165352191f6ee11ed8d8f483e9d5ef46c8c
Victor Churchill <vchurchill2008@gmail.com>
2015-12-04 18:41:20 -0500

    test with tikhonov, M2L computation

--- tikhonov.py
+++
@@ -1,5 +0,0 @@
-# tikhonov regularization parameter
-#alpha = np.power(10, -12)
-#I = np.matlib.identity(p)
-#eqd1u1 = np.dot(np.dot(np.linalg.inv(alpha*I + np.dot(np.matrix.transpose(K3),K3)),np.matrix.transpose(K3)),q1u)
-#eqd2u2 = np.dot(np.dot(np.linalg.inv(alpha*I + np.dot(np.matrix.transpose(K4),K4)),np.matrix.transpose(K4)),q2u)
